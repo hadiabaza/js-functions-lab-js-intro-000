8f9c3023754da1a234a64f6e8174fd255467a82b
Hadi Abaza <hadiabaza1@gmail.com>
2021-04-08 00:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
+function happyCustomHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`;
+}
